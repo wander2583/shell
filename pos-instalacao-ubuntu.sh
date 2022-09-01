5016e68caad2dc91ef1f62fc16f91f4aaa47a6e2
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-09-01 07:23:10 -0300

    Adicionando o arquivo ao repositóro

---
+++ pos-instalacao-ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##############################################################
+# Pós Instalação do Ubuntu para uso pessoal.
+##############################################################
+# Site: github.com/pos-instalacao-ubuntu.sh
+# Autor: Wanderlei Pereira - <wanderlei2583@gmail.com>
+# Manutenção: Wanderlei Pereira - <wanderlei2583@gmail.com>
+##############################################################
+# Este programa faz a configuração pessoal após a intalação
+# do sistema operacional Linux Ubuntu.
+##############################################################
+# V.01 - Iniciando o programa..
+##############################################################
+
+echo pwd
+
+
